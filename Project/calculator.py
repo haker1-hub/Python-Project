179f1cd3a8491c8bd547a1f5700e133c690d3062
BELAL <belnoby191@gmail.com>
2025-12-10 14:58:10 +0200

    update commit

--- Project/calculator.py
+++ Project/calculator.py
@@ -4,50 +4,58 @@ print("="*80)
 print("Calculator".center(80))
 print("="*80)
 
-#====================In put=============================
-
-num1 = float(input("Enter number 1: "))
-num2 = float(input("Enter number 2: "))
-operation = input("""Choose operation:
-1. +
-2. -
-3. *
-4. /
-5. **
-6. //
-7. %
-= """)
-
+#====================In put============================
+num1 = input("Enter number 1: ").strip().capitalize()
 #========================If=============================
+while num1 != "Exit":
 
-if operation in ["1","+"]:
+    num1 = input("Enter number 1: ").strip().capitalize()
+    num2 = float(input("Enter number 2: "))
+    operation = input("""Choose operation:
+    1. +
+    2. -
+    3. *
+    4. /
+    5. **
+    6. //
+    7. %
+    = """)
+    if num1 == "Exit":
 
-    print(num1 + num2)
+        print("Thank you for your time")
 
-elif operation in ["2","-"]:
+        break
 
-    print(num1 - num2)
+    if operation in ["1","+"]:
 
-elif operation in ["3","*"]:
+        print(float(num1) + num2)
 
-    print(num1 * num2)
+    elif operation in ["2","-"]:
 
-elif operation in ["4","/"]:
+        print(float(num1) - num2)
 
-    print("Error" if num2 == 0 else num1 / num2)
+    elif operation in ["3","*"]:
 
-elif operation in ["5","**"]:
+        print(float(num1) * num2)
 
-    print(num1 ** num2)
+    elif operation in ["4","/"]:
 
-elif operation in ["6","//"]:
+        print("Error" if num2 == 0 else float(num1) / num2)
 
-    print("Error" if num2 == 0 else num1 // num2)
+    elif operation in ["5","**"]:
 
-elif operation in ["7",""]:
+        print(float(num1) ** num2)
 
-    print("Error" if num2 == 0 else num1 % num2)
+    elif operation in ["6","//"]:
 
-else:
+        print("Error" if num2 == 0 else float(num1) // num2)
 
-    print("Error: No Operation")
+    elif operation in ["7","%"]:
+
+        print("Error" if num2 == 0 else float(num1) % num2)
+
+    else:
+
+        print("Error:) Try again")
+
+
--- Project/course_pricing.py
+++ Project/course_pricing.py
@@ -4,104 +4,125 @@ print("="*80)
 print("LOG IN".center(80))
 print("="*80)
 
-#===================Informatoin=================
+#===================Informatoin==================
 
-fname = input("Inter your frist name : ").capitalize().strip()
-sname = input("Inter your second name : ").capitalize().strip()
-age = int(input("Inter your real age :").strip())
-gmail = input("Inter your gmail :").strip()
+fname = input("Enter your frist name : ").capitalize().strip()
+sname = input("Enter your second name : ").capitalize().strip()
+age = int(input("Enter your real age :").strip())
+gmail = input("Enter your gmail :").strip()
 st = input("Are you a student ?").strip().capitalize()
-country = input("Inter your country :").strip()
-course=input("Inter course name :\n1.Python\n2.English\n3.Arabic\nHere:").capitalize()
-username=gmail[:gmail.index("@")]
+country = input("Enter your country :").strip().upper()
+course = input("Enter course name :\n1.Python\n2.English\n3.Arabic\nHere:").capitalize()
+username = gmail[:gmail.index("@")]
+password = input("Write your pass word:").strip()
 
+pw ="B1b2b3b4b5"
+tries = 5
 #=================frist print===================
 
 print(f"Hello {fname} {sname}".title().center(40))
 
-#=====================if-student====================
+#====================While======================
 
-if course in ["1","2","Python","English"] :
-    
-    if country == "Egypt" :
+while password != pw:
 
-        if st == "Yes":
+    tries -= 1
+    print(f"Uncorrect password :-) , you have onther {'last' if tries == 0 else tries}tries.")
+    password = input("Write your pass word:").strip()
+ 
+    if tries == 0:
 
-            print(f"Because you from {country} and a student \n you have discount = 50$")
+        break
+ 
+    if password == pw:
+
+        print("correct password")
+
+#=====================if-student================
+
+        if course in ["1","2","Python","English"] :
+            
+            if country == "EGYPT" :
+
+                if st == "Yes":
+
+                    print(f"Because you from {country} and a student \n you have discount = 50$")
+
+                else:
+
+                    print(f"Because you from {country} you have discount = 25$")
+            elif country == "KUWAIT" :
+
+                if st == "Yes":
+
+                    print(f"Because you from {country}  and a student \n you have discount = 20$")
+
+                else:
+
+                    print(f"Because you from {country} you have discount = 5$")
+            
+            elif country == "KSA" :
+
+                if st == "Yes":
+
+                    print(f"Because you from {country}  and a student \n you have discount = 25$")
+
+                else:
+
+                    print(f"Because you from {country} you have discount = 10$")
+
+            else:
+
+                print("Sorry we don't sell course your country")
+
+        elif course in ["Arabic","3"]:
+                
+            if country == "EGYPT" :
+
+                if st == "Yes":
+
+                    print(f"Because you from {country} and a student \n you have discount = 70$")
+
+                else:
+
+                    print(f"Because you from {country} you have discount = 50$")
+            
+            elif country == "KUWAIT" :
+
+                if st == "Yes":
+
+                    print(f"Because you from {country} and a student \n you have discount = 50$")
+
+                else:
+
+                    print(f"Because you from {country} you have discount = 25$")
+            
+            elif country == "KSA" :
+
+                if st == "Yes":
+
+                    print(f"Because you from {country} and a student \n you have discount = 55$")
+
+                else:
+
+                    print(f"Because you from {country} you have discount = 30$")
+
+            else:
+                
+
+                print("Sorry we don't sell course your country")
+
+#=====================if-age====================
+
+        if age >= 18 :
+            
+            print(f"Log in successful  {username} with your gmail {gmail}")
 
         else:
 
-            print(f"Because you from {country} you have discount = 25$")
-    elif country == "Kuwait" :
+            print(f"Don't allow to log in under limited age \n You can login after {18-age} years .")
+            
+#=======================final===================
 
-        if st == "Yes":
+        print(f"Thank you for your time {fname}")
 
-            print(f"Because you from {country}  and a student \n you have discount = 20$")
-
-        else:
-
-            print(f"Because you from {country} you have discount = 5$")
-    
-    elif country == "KSA" :
-
-        if st == "Yes":
-
-            print(f"Because you from {country}  and a student \n you have discount = 25$")
-
-        else:
-
-            print(f"Because you from {country} you have discount = 10$")
-
-    else:
-
-        print("Sorry we don't sell course your country")
-
-elif course in ["Arabic","3"]:
-        
-    if country == "Eygpt" :
-
-        if st == "Yes":
-
-            print(f"Because you from {country} and a student \n you have discount = 80$")
-
-        else:
-
-            print(f"Because you from {country} you have discount = 50$")
-    elif country == "Kuwait" :
-
-        if st == "Yes":
-
-            print(f"Because you from {country} and a student \n you have discount = 50$")
-
-        else:
-
-            print(f"Because you from {country} you have discount = 25$")
-    
-    elif country == "KSA" :
-
-        if st == "Yes":
-
-            print(f"Because you from {country} and a student \n you have discount = 55$")
-
-        else:
-
-            print(f"Because you from {country} you have discount = 30$")
-
-    else:
-        
-
-        print("Sorry we don't sell course your country")
-
-#=====================if-age======================
-
-if age >= 18 :
-    
-    print(f"Log in successful  {username} with your gmail {gmail}")
-
-else:
-
-    print(f"Don't allow to log in under limited age \n You can login after {18-age} years .")
-    
-#=======================final=====================
-
-print(f"Thank you for your time {fname}")
